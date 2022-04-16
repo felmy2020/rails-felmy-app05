f01c5597998d66d4bd41865ae57bd10852d1917d
felmy2020 <fyen.work@gmail.com>
2022-04-16 19:25:50 +0900

    rSpecを導入

--- src/config/application.rb
+++ src/config/application.rb
@@ -10,7 +10,8 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.time_zone = 'Asia/Tokyo' #追加
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
